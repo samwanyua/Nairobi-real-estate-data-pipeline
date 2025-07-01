f289f74e0832bb96f9270ef27ce54d848735da7b
samwanyua <samexample8@gmail.com>
2025-07-01 16:04:43 +0300

    update kafka stream to postgres script

--- spark/kafka_stream_to_postgres.py
+++ spark/kafka_stream_to_postgres.py
@@ -1,31 +1,33 @@
-# kafka_consumer/consumer.py
 from kafka import KafkaConsumer
 import psycopg2
 import json
-import os
 
-consumer = KafkaConsumer(
-    'property24_listings',
-    bootstrap_servers='kafka:29092',
-    value_deserializer=lambda m: json.loads(m.decode('utf-8'))
-)
+def consume_and_insert():
+    consumer = KafkaConsumer(
+        'property24_listings',
+        bootstrap_servers='kafka:29092',
+        value_deserializer=lambda m: json.loads(m.decode('utf-8'))
+    )
 
-conn = psycopg2.connect(
-    dbname="clean_db",
-    user="postgres",
-    password="postgres",
-    host="clean_db",
-    port="5432"
-)
-cur = conn.cursor()
+    conn = psycopg2.connect(
+        dbname="clean_db",
+        user="postgres",
+        password="postgres",
+        host="clean_db",
+        port="5432"
+    )
+    cur = conn.cursor()
 
-for msg in consumer:
-    data = msg.value
-    print("Received:", data)
+    for msg in consumer:
+        data = msg.value
+        print(f"Inserting: {data}")
+        cur.execute("""
+            INSERT INTO cleaned_listings (id, title, price, location)
+            VALUES (%s, %s, %s, %s)
+            ON CONFLICT (id) DO NOTHING
+        """, (data["id"], data["title"], data["price"], data["location"]))
+        conn.commit()
+        break  # Avoid running forever during Airflow DAG runs
 
-    cur.execute("""
-        INSERT INTO cleaned_listings (id, title, price, location)
-        VALUES (%s, %s, %s, %s)
-        ON CONFLICT (id) DO NOTHING;
-    """, (data['id'], data['title'], data['price'], data['location']))
-    conn.commit()
+    cur.close()
+    conn.close()
